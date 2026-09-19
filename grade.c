cb0d494dd17c3a8b45f2cfea2f6cb3cad46c31d5
cattyestrao-del <cattyestrao@gmail.com>
2026-09-19 20:46:31 +0800

    Create grade.c

---
+++ grade.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+ int main() {
+     int score;
+     printf("Enter your score (0-100): ");
+     scanf("%d", &score);
+     if (score < 0 || score > 100) {
+         printf("Invalid score! Range is 0-100\n");
+     }
+     else if (score >= 90) {
+         printf("Score: %d → Grade: A\n", score);
+     }
+     else if (score >= 80) {
+         printf("Score: %d → Grade: B\n", score);
+     }
+     else if (score >= 70) {
+         printf("Score: %d → Grade: C\n", score);
+     }
+     else if (score >= 60) {
+         printf("Score: %d → Grade: D\n", score);
+     }
+     else {
+         printf("Score: %d → Grade: F\n", score);
+     }
+     return 0;
+ }
