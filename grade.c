95396c376e8f1dc3fb41a078b384aafcd7cd4e09
cattyestrao-del <cattyestrao@gmail.com>
2026-09-19 20:55:05 +0800

    Update grade.c

--- grade.c
+++ grade.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
  int main() {
      int score;
-     printf("Enter your score (0-100): ");
+     printf("Enter your score (0-100):");
      scanf("%d", &score);
 
      if (score < 0 || score > 100) {
@@ -9,19 +9,19 @@
      printf("Invalid score! Range is 0-100\n");
      }
      else if (score >= 90) {
-         printf("Score: %d → Grade: A\n", score);
+         printf("Score: %d Grade:A\n", score);
      }
      else if (score >= 80) {
-         printf("Score: %d → Grade: B\n", score);
+         printf("Score: %d Grade:B\n", score);
      }
      else if (score >= 70) {
-         printf("Score: %d → Grade: C\n", score);
+         printf("Score: %d Grade: C\n", score);
      }
      else if (score >= 60) {
-         printf("Score: %d → Grade: D\n", score);
+         printf("Score: %d Grade: D\n", score);
      }
      else {
-         printf("Score: %d → Grade: F\n", score);
+         printf("Score: %d Grade: F\n", score);
      }
      return 0;
  }
